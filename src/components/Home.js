0609d4532cc1e8c95efbb15262d9fd6c30ab7d0a
Raja Prem Sai <premsaimaddula342@gmail.com>
2024-09-30 21:28:14 +0530

    1st commmit

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TopBar from "./TopBar";
+import Dashboard from "./Dashboard";
+
+function Home() {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+}
+
+export default Home;
+<>
+  <TopBar />
+  <Dashboard />
+</>;
